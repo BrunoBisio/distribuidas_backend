9632f18a7fedf2a001b3247de0902a8a09131b60
Bruno Bisio <brunobisio@WKSAR1029L.local>
2021-06-01 21:13:59 -0300

    AzureSQL connection added

--- src/main/java/distribuidas/backend/models/Photo.java
+++ src/main/java/distribuidas/backend/models/Photo.java
@@ -1,7 +1,6 @@
 package distribuidas.backend.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -20,7 +19,12 @@ public class Photo {
         )
         go
     */
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "identificador", nullable = false)
     private int id;
+    @OneToOne
+    @JoinColumn(name = "producto")
     private Product product;
     private String uri;
 }
