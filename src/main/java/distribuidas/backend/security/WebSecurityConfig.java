d5f939023e0dde124162ab8ab5387e7b64093c0b
alexis <alexis.joel.sessarego@gmail.com>
2021-06-06 21:33:29 +0200

    securityfix

--- src/main/java/distribuidas/backend/security/WebSecurityConfig.java
+++ src/main/java/distribuidas/backend/security/WebSecurityConfig.java
@@ -59,7 +59,10 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
                 // dont authenticate this particular request
                 .authorizeRequests().antMatchers(HttpMethod.POST,"/login", "/user").permitAll().
                 // all other requests need to be authenticated
-                        anyRequest().authenticated().and().
+                // /auction /auction/soon /auction/id
+                        antMatchers("/auction/user").authenticated().
+                        antMatchers("/auction", "/auction/soon", "/auction/{\\d+}").permitAll().
+                anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
                 // store user's state.
                         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
