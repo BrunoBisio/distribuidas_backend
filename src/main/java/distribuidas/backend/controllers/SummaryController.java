9b31b0f8089c262e18c78811c58ee6cb9d526014
Bruno Bisio <bisioba29@gmail.com>
2021-06-25 20:50:47 -0300

    se agrego export de subastas participadas

--- src/main/java/distribuidas/backend/controllers/SummaryController.java
+++ src/main/java/distribuidas/backend/controllers/SummaryController.java
@@ -1,11 +1,20 @@
 package distribuidas.backend.controllers;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import distribuidas.backend.dtos.AuctionsWonDto;
 import distribuidas.backend.dtos.BidsCreatedDto;
@@ -27,6 +36,31 @@ public class SummaryController {
     @GetMapping("/auctions/assisted/details")
     public List<ParticipatedAuctionDto> getAuctionedAuctionsDetails() { return service.getAuctionedAuctionsDetails(Context.getPrincipalId()); }
     
+    @GetMapping("/auctions/assisted/details/export")
+    public void exportAuctionedAuctionsDetails(HttpServletResponse response) throws IOException { 
+        response.setContentType("text/csv");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String currentDateTime = dateFormatter.format(new Date());
+         
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=subastasParticipadas_" + currentDateTime + ".csv";
+        response.setHeader(headerKey, headerValue);
+         
+        List<ParticipatedAuctionDto> auctions = service.getAuctionedAuctionsDetails(Context.getPrincipalId());
+ 
+        ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+        String[] csvHeader = {"Fecha", "Categoria", "Moneda", "Subasta"};
+        String[] nameMapping = {"auctionDate", "category", "currency", "auctionName"};
+         
+        csvWriter.writeHeader(csvHeader);
+         
+        for (ParticipatedAuctionDto auction : auctions) {
+            csvWriter.write(auction, nameMapping);
+        }
+         
+        csvWriter.close();
+    }
+
     // subastas en las que gano
     @GetMapping("/auctions/won")
     public long getAuctionsWon() { return service.getAuctionsWon(Context.getPrincipalId()); }
